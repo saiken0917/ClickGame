6bef6c20a431504be779d93ffc4b9c3e7e3dfc84
saiken0917 <saiken0917@gmail.com>
2017-12-14 21:49:58 +0800

    revised at 20171214

---
+++ ClickGame-Startup/ClickGame-Startup/Assets/Scripts/Views/EnemyBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]//必須有Animator
+[RequireComponent(typeof(MeshFader))]//必須有MeshFader
+[RequireComponent(typeof(AudioSource))]//必須有MeshFader
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+    private Animator animator;
+    private MeshFader meshFader;
+    private AudioSource audioSource;
+    // Use this for initialization
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(meshFader.FadeIn());//一進來就淡入
+    }
+
+    [ContextMenu("Test")]//供測試用
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))//when clicked leftMouse
+            DoDamage(10);//造成10傷害      
+    }
+
+    public void DoDamage(int attack)
+    {
+        animator.SetTrigger("hurt");//受到傷害，觸發"hurt"動畫
+        audioSource.Play();//播放受傷音效
+    }
+
+    public IEnumerator Execute()
+    {
+        yield return null; //先回傳空值始可Compiler
+    }
+}
---
+++ ClickGame-Startup/ClickGame-Startup/Assets/Scripts/Views/MeshFader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFader : MonoBehaviour {
+    //淡入淡出
+    public Renderer[] fadeRenderers;//陣列
+    public float speed = 1;//設定速度為1
+
+    [ContextMenu("Test FadeIn")]//加上一個"Test FadeIn"選項在此腳本的齒輪選項中
+    public void TestFadeIn()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    [ContextMenu("Test FadeOut")]//加上一個"Test FadeOut"選項在此腳本的齒輪選項中
+    public void TestFadeOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    public IEnumerator FadeIn()
+    {
+        StopCoroutine(FadeOut());//執行前先停止FadeOut
+        float alpha = 0;//設定alpha為0
+        ChangeAlpha(alpha);//重複的程式碼做成方法,並傳區域變數進去
+
+        while (alpha < 1)//淡入
+        {
+            alpha += Time.deltaTime * speed;
+            alpha = Mathf.Min(alpha, 1);//讓alpha永遠不會超過1
+            ChangeAlpha(alpha);//重複的程式碼做成方法,並傳區域變數進去
+            yield return null;//回傳空值後再次進來執行一次
+        }
+    }
+
+    public IEnumerator FadeOut()
+    {
+        StopCoroutine(FadeIn());//執行前先停止FadeIn
+        float alpha = 1;//設定alpha為1
+        ChangeAlpha(alpha);//重複的程式碼做成方法,並傳區域變數進去
+        while (alpha > 0)//淡出
+        {
+            alpha -= Time.deltaTime * speed;
+            alpha = Mathf.Max(0, alpha);//讓alpha永遠不會小於0
+            ChangeAlpha(alpha);//重複的程式碼做成方法,並傳區域變數進去
+            yield return null;//回傳空值後再次進來執行一次
+        }
+    }
+
+    private void ChangeAlpha(float alpha) //Ctrl + R 兩次 = 統一重新命名
+    {
+        for (int i = 0; i < fadeRenderers.Length; i++)
+        {
+            //fadeRenderers[i].material.color.a = alpha;(無法直接改，必須先宣告再填回去)
+            Color color = fadeRenderers[i].material.color;
+            color.a = alpha;
+            fadeRenderers[i].material.color = color;
+        }
+    }
+}
